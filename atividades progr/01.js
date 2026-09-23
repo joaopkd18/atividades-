17f4b6198516890f7dd43a6e2e5edd5935137e4a
João 𓆝 𓆟 𓆞 𓆝 <melojoaopaulo812@gmail.com>
2026-09-23 14:18:33 -0300

    Add files via upload

---
+++ atividades progr/01.js
@@ -0,0 +1,7 @@
+function calcuarAreRetangulo() {
+    let  base = Number(prompt ("Digite a base : "))
+let altura = Number(prompt("Digite a altura:"))
+    let area = base*altura
+    return alert ("A area do retangulo e: "+ area ) 
+}
+calcuarAreRetangulo()
